8cd55d3c51a5440359554677803aad7e65561d31
Kezia666 <a.morozova123@gmail.com>
2020-08-27 12:33:48 +0300

    Task 4 was added

---
+++ task-4.py
@@ -0,0 +1,14 @@
+while True:
+    n = int(input("Введите целое положительное число: "))
+    if n > 0:
+        digit = 0
+
+        while True:
+            if n % 10 > digit:
+                digit = n % 10
+            if n < 10:
+                break
+            n = n // 10
+
+        print(f"Самая большая цифра в вашем числе - {digit}")
+        break
