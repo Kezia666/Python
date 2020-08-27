44f8a72a21c809247820732d2921c76ef7aed2ad
Kezia666 <a.morozova123@gmail.com>
2020-08-27 19:19:50 +0300

    Task 3 was updated

--- task-3.py
+++ task-3.py
@@ -1,5 +1,7 @@
-n = int(input("Введите число n: "))
-nn = int(f"{n}{n}")
-nnn = int(f"{n}{n}{n}")
-
-print(n + nn + nnn)
+while True:
+    n = int(input("Введите число n: "))
+    if n > 0:
+        nn = int(f"{n}{n}")
+        nnn = int(f"{n}{n}{n}")
+        print(n + nn + nnn)
+        break
