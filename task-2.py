7790527a7192bcebcfa57d604f670bbf02f7082c
Kezia666 <a.morozova123@gmail.com>
2020-08-28 12:47:01 +0300

    Task 2 was updated and task 6 was added

--- task-2.py
+++ task-2.py
@@ -1,25 +1,8 @@
 while True:
     sec = int(input("Введите время в секундах (число от 0 до 86399): "))
     if 0 <= sec <= 86399:
-
-        # Condition for hh
-        if sec // 3600 < 10:
-            hh = f"0{str(sec // 3600)}"
-        else:
-            hh = f"{str(sec // 3600)}"
-
-        # Condition for mm
-        if sec % 3600 // 60 < 10:
-            mm = f"0{str(sec % 3600 // 60)}"
-        else:
-            mm = f"{str(sec % 3600 // 60)}"
-
-        # Condition for ss
-        if sec % 3600 % 60 < 10:
-            ss = f"0{str(sec % 3600 % 60)}"
-        else:
-            ss = f"{str(sec % 3600 % 60)}"
-
-        # Output
-        print(f"{hh}:{mm}:{ss}")
+        hh = sec // 3600
+        mm = sec % 3600 // 60
+        ss = sec % 3600 % 60
+        print(f"Время в формате (чч:мм:сс): {hh:02d}:{mm:02d}:{ss:02d}")
         break
